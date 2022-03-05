c4fb4cab60248e5bc4b1b1951e29d7f76148e950
brentont240 <60454698+brentont240@users.noreply.github.com>
2022-03-05 16:15:22 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+// Database Setup
+// Connect to MongoDB
+// TODO: PUT THE USERNAME AND PASSWORD IN ENVIRONEMENT VARIABLES: LOOK AT SUPER BRACKETS FOR MORE INFO!!!
+const username = process.env.SCAM_SENTRY_USERNAME;
+const password = process.env.SCAM_SENTRY_PASSWORD;
+
+// TODO: REMOVE PASS BEFORE PUBLISHING!!! (between the : and @)
+
+const MONGODB_URL = process.env.MONGODB_URL || `mongodb+srv://${username}:${password}@scamsentrycluster.urdcn.mongodb.net/Tools`;
+
+const app = express();
+
+// TODO: host the backend on heroku and the front end on something else!!!
+
+// TODO: SHOULD I PUT /api/ in front of the routes?
+
+const emailDetectorRoutes = require('./routes/emailDetector');
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // Needed to read the body. (instead of body parser, body parser is depricated)
+app.use(emailDetectorRoutes);
+
+
+mongoose
+    .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connected to MongoDB");
+     })
+     .catch((err) => {
+        console.log(err);
+     });
+
+app.listen(PORT, () => console.log('Server connected at port:', PORT));
