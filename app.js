b1cb1199214ccace39e863c53eee0b6fe3b8fa9c
Brenton <brentontreb@gmail.com>
2022-03-05 18:38:09 -0700

    Added cors options for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // Database Setup
 // Connect to MongoDB
@@ -12,7 +13,14 @@ const password = process.env.SCAM_SENTRY_PASSWORD;
 
 const MONGODB_URL = process.env.MONGODB_URL || `mongodb+srv://${username}:${password}@scamsentrycluster.urdcn.mongodb.net/Tools`;
 
+
+const corsOptions = {
+   origin: "https://proveassignmentsbrenton.herokuapp.com/",
+   optionsSuccessStatus: 200
+ };
+
 const app = express();
+
 
 // TODO: host the backend on heroku and the front end on something else!!!
 
@@ -24,6 +32,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json()); // Needed to read the body. (instead of body parser, body parser is depricated)
 app.use(emailDetectorRoutes);
+app.use(cors(corsOptions));
 
 
 mongoose
