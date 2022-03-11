4ef38fbddb61d2536bc1e6f08cb32bb77e5c4a37
Brenton <brentontreb@gmail.com>
2022-03-10 17:38:40 -0700

    Fixed problem with guru detector

--- controllers/guruDetector.js
+++ controllers/guruDetector.js
@@ -19,44 +19,25 @@ exports.checkGuru = (req, res, next) => {
             websites.push(website);
         });
       });
-    //   TODO: get this to work!!!
       const websiteMatch = checkMatch(userInput, websites);
       const guruMatch = checkMatch(userInput, gurus);
 
-      // if(websiteMatch != null){
-      //   matchFound = true;
-      //   res.status(200).json({ matchFound, websiteMatch });
-      // } else if (guruMatch != null){
-      //   matchFound = true;
-      //   res.status(200).json({ matchFound, guruMatch });
-      // } else
-
-      // if (websiteMatch != null || guruMatch != null)
-      //   matchFound = true;
-        // res.status(200).json({ gurus, websites });
-        // res.status(200).json({ websiteMatch, guruMatch });
-        res.status(200).json({matchFound, websiteMatch, guruMatch});
-        // res.status(200).json({matchFound, websiteMatch});
+      res.status(200).json({matchFound, websiteMatch, guruMatch});
     })
     .catch((err) => {
       console.log(err);
     });
 };
 
-// FIXME: get this to work
 function checkMatch(input, source){
     let match = null;
     source.forEach((keyword) => {
+      // need to check if null, can't do toLowerCase on null
       if (keyword != null && input.includes(keyword.toLowerCase())) {
         match = keyword;
         matchFound = true;
         return match;
-        // break;
       }
     });
-    // if(match !== ""){
-    //     return match;
-    // }
-    // else return "";
     return match;
 }
