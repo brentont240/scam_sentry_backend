284b13f148eff57f18496cc01df9c333e61635f1
Brenton <brentontreb@gmail.com>
2022-03-15 23:33:32 -0600

    Finishing up mlm detector

--- controllers/mlmDetector.js
+++ controllers/mlmDetector.js
@@ -9,9 +9,14 @@ let matchFound = false;
 let match = null;
 let type = null;
 
+// TODO:
+// TODO:
+// TODO: GET THIS TO WORK THEN UNCOMMENT THE ROUTE THING IN APP.JS!!!
+
+
 exports.checkCompany = (req, res, next) => {
     let userInput = (req.body.input).toLowerCase();
-    // console.log(userInput);
+    console.log(userInput);
     let companys = [];
     Mlm_List.find()
     .then((databaseList) => {
@@ -26,21 +31,21 @@ exports.checkCompany = (req, res, next) => {
     });
 };
 
-// function checkMatch(input, mlmList){
-//     let currMlm = '';
-//     mlmList.forEach((mlm) => {
-//       currMlm = mlm.company;
-//       // remove parenthesis to check the match, if there exists parenthesis
-//       if (currMlm.includes('(')){
-//         currMlm = currMlm.substring(0,currMlm.indexOf('(')-1);
-//       }
-//       // need to check if null, can't do toLowerCase on null
-//       if (input.includes(currMlm.toLowerCase())) {
-//         match = mlm.company;
-//         matchFound = true;
-//         type = mlm.type;
-//         return;
-//       }
-//     });
-//     return;
-// }
+function checkMatch(input, mlmList){
+    let currMlm = '';
+    mlmList.forEach((mlm) => {
+      currMlm = mlm.company;
+      // remove parenthesis to check the match, if there exists parenthesis
+      if (currMlm.includes('(')){
+        currMlm = currMlm.substring(0,currMlm.indexOf('(')-1);
+      }
+      // need to check if null, can't do toLowerCase on null
+      if (input.includes(currMlm.toLowerCase())) {
+        match = mlm.company;
+        matchFound = true;
+        type = mlm.type;
+        return;
+      }
+    });
+    return;
+}
